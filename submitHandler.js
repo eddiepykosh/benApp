38c0c9cd3343ec4b79deafdc791c1eaf470662c1
eddie <eddie@pykosh.com>
2023-07-24 20:18:40 -0400

    regex moment

--- submitHandler.js
+++ submitHandler.js
@@ -3,24 +3,36 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 const phoneNumber = process.env.TWILIO_PHONE_NUMBER;
+
 // This function will be called when the "btnSubmit" button is clicked
 function handleSubmission(text, req) {
-    // Log the content of the "textbox" to the console
-    console.log('Textbox content:', text);
-    // Get the requester's IP address from the request object
-    const ipAddress = req.ip;
-    console.log('Requester IP Address:', ipAddress);
-  
-    // You can add any other server-side logic here if needed
-    client.calls
-    .create({
-       url: ' http://demo.twilio.com/docs/classic.mp3',
-       from: phoneNumber,
-       to: text,
-       method: 'GET'
-     })
-    .then(call => console.log(call.sid));   
+  // Log the content of the "textbox" to the console
+  console.log('Textbox content:', text);
+
+  // Get the requester's IP address from the request object
+  const ipAddress = req.ip;
+  console.log('Requester IP Address:', ipAddress);
+
+  // Regular expression to validate 10-digit phone numbers
+  const phoneRegex = /^\d{10}$/;
+
+  // Check if the input text matches the phone number format
+  if (!phoneRegex.test(text)) {
+    console.log('Invalid phone number. Please enter a 10-digit number.');
+    return; // Exit the function if the phone number is invalid
   }
-  
-  module.exports = handleSubmission;
+
+  // You can add any other server-side logic here if needed
+  client.calls
+    .create({
+      url: 'http://demo.twilio.com/docs/classic.mp3',
+      from: phoneNumber,
+      to: text,
+      method: 'GET'
+    })
+    .then(call => console.log(call.sid));
+}
+
+module.exports = handleSubmission;
+
   
